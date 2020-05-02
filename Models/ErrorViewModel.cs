a5f2eb307ee92c80669419d35e59997805ce7a1d
Julio Macal <julio.macal@catolica.edu.sv>
2020-05-01 21:05:37 -0600

    Cargando todos los archivos

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace unicaes2011ml604.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
